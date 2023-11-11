268f3bb37208257d4852062285b7e5f7b3774e71
Chandra Lingam <20747492+ChandraLingam@users.noreply.github.com>
2023-11-11 05:29:40 -0800

    Updates

--- InteractiveTool/LearnRegex.py
+++ InteractiveTool/LearnRegex.py
@@ -1,9 +1,6 @@
 # Python Regular Expressions - GUI Interactive Tool  
 #   Chandra Lingam
-#   Cloud Wave LLC
-#   
-#   Compatible with 3.x version of python
-# Distributed under GNU Public License - https://www.gnu.org/licenses/gpl-3.0.en.html
+#   Compute With Cloud Inc
 
 # Version History
 # Original Version - Chandra Lingam
